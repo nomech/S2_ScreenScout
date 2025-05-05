4aef5e4b023a19298d2d1cb47e0f403a94c15b7d
nomech <mech1803@gmail.com>
2025-05-05 09:22:09 +0200

    Add Navbar component and integrate it

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import { Outlet } from "react-router-dom";
 import "./App.css";
 import { useContext } from "react";
 import { authContext } from "./context/authContext";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   const user = useContext(authContext);
   console.log(user);
   console.log(user.displayname);
-  
 
-  return <Outlet />;
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
