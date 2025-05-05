7f7b4ac387ddca339fb55f178d79731bf23434c6
Christoffer <mech1803@gmail.com>
2025-05-05 20:25:29 +0200

    Refactor HomePage component to include session management and API fetching logic

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,7 +1,67 @@
 import React from "react";
+import { useFetch } from "../../hooks/useFetch";
 
-const Home = () => {
-  return <div>Home</div>;
+const HomePage = () => {
+  const { data } = useFetch(
+    "https://api.themoviedb.org/3/trending/all/day?language=en-US"
+  );
+
+  const sessionTest = async () => {
+    try {
+      const options = {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOGM2MDMwZTU1OGY1NDViOGMwNTI5ODY0MTY1NDNmYSIsIm5iZiI6MTY3MTU2MTk2Ny43NCwic3ViIjoiNjNhMjAyZWY4ZGRjMzQxNGFhMDMwZjMyIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.3g3McMn73fcJa7Fj7a1ElttZTa44HgldypZVhKFHNBo",
+        },
+      };
+      const response = await fetch(
+        "https://api.themoviedb.org/3/authentication/token/new",
+        options
+      );
+
+      const data = await response.json();
+      console.log(data);
+      
+      return data;
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const handleClick = async () => {
+    const session = await sessionTest();
+
+    const options = {
+      method: 'POST',
+      headers: {
+        accept: 'application/json',
+        'content-type': 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOGM2MDMwZTU1OGY1NDViOGMwNTI5ODY0MTY1NDNmYSIsIm5iZiI6MTY3MTU2MTk2Ny43NCwic3ViIjoiNjNhMjAyZWY4ZGRjMzQxNGFhMDMwZjMyIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.3g3McMn73fcJa7Fj7a1ElttZTa44HgldypZVhKFHNBo'
+      },
+      body: JSON.stringify({ request_token: session }),
+    };
+    try {
+      const response = await fetch(
+        "https://api.themoviedb.org/3/authentication/session/new",
+        options
+      );
+
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  
+  return (
+    <div>
+      <p>Session Test</p>
+      <button onClick={handleClick}>Test Button</button>
+    </div>
+  );
 };
 
-export default Home;
+export default HomePage;
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,7 +1,47 @@
-const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization: `${import.meta.env.VITE_APP_TMDB_API}`,
-  },
+import { useEffect, useState } from "react";
+
+export const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `${import.meta.env.VITE_APP_TMDB_TOKEN}`,
+      },
+      signal: controller.signal,
+    };
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setData(data);
+        setError(null);
+        console.log(data);
+      } catch (error) {
+        console.error("Fetch error:", error);
+        throw error;
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    if (url) {
+      setIsLoading(true);
+      fetchData();
+      return () => {
+        controller.abort();
+      };
+    }
+  }, [url]);
+
+  return { data, error, isLoading };
 };
