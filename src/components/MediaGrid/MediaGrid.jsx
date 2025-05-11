2df454c0b9529ed36d868638686976e9a73b1568
nomech <mech1803@gmail.com>
2025-05-11 23:37:50 +0200

    Add card style toggle functionality to MediaGrid component and update CSS for grid and list views

--- src/components/MediaGrid/MediaGrid.jsx
+++ src/components/MediaGrid/MediaGrid.jsx
@@ -1,10 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./MediaGrid.module.css";
 import { useFetch } from "../../hooks/useFetch";
 import Loading from "../Loading/Loading";
 
 const MediaGrid = ({ limit, title, setMatches, getTotalPages, url }) => {
   const { data, isLoading } = useFetch(url);
+  const [cardStyle, setCardStyle] = useState("List");
+
   const items = (data?.results || []).filter(
     (media) => media.media_type !== "person"
   );
@@ -15,6 +17,15 @@ const MediaGrid = ({ limit, title, setMatches, getTotalPages, url }) => {
     }
   }, [setMatches, getTotalPages, data, isLoading]);
 
+  const setListStyle = () => {
+    setCardStyle("List");
+  };
+
+  const setGridStyle = () => {
+    setCardStyle("Grid");
+  };
+
+  console.log(cardStyle);
   return (
     <>
       {isLoading && <Loading />}
@@ -23,11 +34,16 @@ const MediaGrid = ({ limit, title, setMatches, getTotalPages, url }) => {
         <div className={styles.title}>
           <h1 className={styles.title}>{title}</h1>
         </div>
-        <div className={styles.mediaGrid}>
+        <div
+          className={`${styles.mediaGrid} ${styles["container" + cardStyle]}`}
+        >
           {data &&
             items?.media_type != "person" &&
             items.slice(0, limit).map((media) => (
-              <div key={media.id} className={styles.card}>
+              <div
+                key={media.id}
+                className={`${styles.card} ${styles["card" + cardStyle]}`}
+              >
                 <img
                   className="poster"
                   src={`https://image.tmdb.org/t/p/w200${media.poster_path}`}
