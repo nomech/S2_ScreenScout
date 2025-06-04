45f4c96eb820a124bf7573d6e4a8d38027a1e74f
Christoffer <mech1803@gmail.com>
2025-06-05 01:24:31 +0200

    Add error handling for forgot password

--- src/components/ForgotPasswordCard/ForgotPasswordCard.jsx
+++ src/components/ForgotPasswordCard/ForgotPasswordCard.jsx
@@ -1,126 +1,124 @@
-import styles from "./ForgotPasswordCard.module.css";
-import React, { useEffect, useState } from "react";
-import Input from "../Input/Input";
-import Button from "../Button/Button";
-import { forgotPasswordFormValidation } from "../../utils/formValidation";
-import { sendPasswordResetEmail } from "firebase/auth";
-import { auth } from "../../firebaseConfig";
-import { NavLink, useNavigate } from "react-router-dom";
+import styles from './ForgotPasswordCard.module.css';
+import React, { useEffect, useState } from 'react';
+import Input from '../Input/Input';
+import Button from '../Button/Button';
+import { forgotPasswordFormValidation } from '../../utils/formValidation';
+import { sendPasswordResetEmail } from 'firebase/auth';
+import { auth } from '../../firebaseConfig';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { formatFireBaseFeedback } from '../../utils/formatFirebaseErrors';
+import InfoCard from '../InfoCard/InfoCard';
 
 // This component provides a form for users to reset their password by entering their email address.
 const ForgotPasswordCard = () => {
-    // Hooks to manage state and side effects
-    const navigate = useNavigate();
-    const [email, setEmail] = useState({ email: "" });
-    const [error, setError] = useState(null);
-    const [success, setSuccess] = useState(null);
-    const [isLoading, setIsloading] = useState(false);
+	// Hooks to manage state and side effects
+	const navigate = useNavigate();
+	const [email, setEmail] = useState({ email: '' });
+	const [error, setError] = useState(null);
+	const [success, setSuccess] = useState(null);
+	const [isLoading, setIsloading] = useState(false);
 
-    // Effect to clear error and success messages after a delay
-    useEffect(() => {
-        let timer;
-        if (success) {
-            timer = setTimeout(() => navigate("/login"), 3000);
-        }
-        return () => clearTimeout(timer);
-    }, [success, navigate]);
+	// Effect to clear error and success messages after a delay
+	useEffect(() => {
+		let timer;
+		if (success) {
+			timer = setTimeout(() => navigate('/login'), 3000);
+		}
+		return () => clearTimeout(timer);
+	}, [success, navigate]);
 
-    // Effect to clear error message after a delay
-    const handleOnChange = (e) => {
-        const { name, value } = e.target;
-        setEmail((prevData) => ({
-            ...prevData,
-            [name]: value,
-        }));
-    };
+	// Effect to clear error message after a delay
+	const handleOnChange = (e) => {
+		const { name, value } = e.target;
+		setEmail((prevData) => ({
+			...prevData,
+			[name]: value,
+		}));
+	};
 
-    // Function to handle form submission for password reset
-    const onSubmit = async (e) => {
-        e.preventDefault();
+	// Function to handle form submission for password reset
+	const onSubmit = async (e) => {
+		e.preventDefault();
 
-        // Validate the email input before sending the reset link
-        if (!forgotPasswordFormValidation(email, setError)) {
-            return;
-        }
+		// Validate the email input before sending the reset link
+		if (!forgotPasswordFormValidation(email, setError)) {
+			return;
+		}
 
-        // Reset error and success messages
+		// Reset error and success messages
 
-        try {
-            setIsloading(true);
-            // Attempt to send a password reset email
-            await sendPasswordResetEmail(auth, email.email);
+		try {
+			setIsloading(true);
+			// Attempt to send a password reset email
+			await sendPasswordResetEmail(auth, email.email);
 
-            // If successful, set success message and clear email input
-            setSuccess(
-                `A password reset link was sent to ${email.email} if it exists`
-            );
-            setEmail({ email: "" });
-        } catch (error) {
-            // If an error occurs, set the error message
-            setError(error.message);
-        } finally {
-            // Reset loading state regardless of success or failure
-            setIsloading(false);
-        }
-    };
+			// If successful, set success message and clear email input
+			setSuccess(`A password reset link was sent to ${email.email} if it exists`);
+			setEmail({ email: '' });
+		} catch (err) {
+			// If an error occurs, set the error message
+			formatFireBaseFeedback(err.code, setError);
+		} finally {
+			// Reset loading state regardless of success or failure
+			setIsloading(false);
+		}
+	};
 
-    return (
-        // Render the forgot password form
-        <form
-            className={styles.forgotPasswordCard}
-            onSubmit={(e) => onSubmit(e)}
-        >
-            <h1 className={styles.title}> ScreenScout</h1>
+	return (
+		// Render the forgot password form
+		<form className={styles.forgotPasswordCard} onSubmit={(e) => onSubmit(e)}>
+			<h1 className={styles.title}> ScreenScout</h1>
 
-            {/* Display error message if there is an error */}
-            {!success && (
-                <>
-                    <div className={styles.inputContainer}>
-                        {/*input for email address with validation*/}
-                        <Input
-                            label="Email"
-                            placeholder="Enter your email"
-                            className="email"
-                            type="email"
-                            value={email.email}
-                            name={"email"}
-                            id="email"
-                            onChange={handleOnChange}
-                            error={error}
-                        />
-                    </div>
+			{/* Display error message if there is an error */}
+			{!success && (
+				<>
+					<div className={styles.inputContainer}>
+						{/*input for email address with validation*/}
+						<Input
+							label="Email"
+							placeholder="Enter your email"
+							className="email"
+							type="email"
+							value={email.email}
+							name={'email'}
+							id="email"
+							onChange={handleOnChange}
+							error={error}
+						/>
+					</div>
 
-                    {/* Links to create an account or sign in */}
-                    <div className={styles.linkContainer}>
-                        <NavLink to="/signup" className={styles.link}>
-                            Create Account
-                        </NavLink>
+					{/* Links to create an account or sign in */}
+					<div className={styles.linkContainer}>
+						<NavLink to="/signup" className={styles.link}>
+							Create Account
+						</NavLink>
 
-                        <NavLink to="/login" className={styles.link}>
-                            Sign in
-                        </NavLink>
-                    </div>
+						<NavLink to="/login" className={styles.link}>
+							Sign in
+						</NavLink>
+					</div>
 
-                    <Button
-                        ariaLabel="Reset button"
-                        className="login"
-                        disabled={isLoading}
-                        type="submit"
-                    >
-                        {isLoading ? "Sending link..." : "Reeset Password"}
-                    </Button>
-                </>
-            )}
+					<Button
+						ariaLabel="Reset button"
+						className="login"
+						disabled={isLoading}
+						type="submit"
+					>
+						{isLoading ? 'Sending link...' : 'Reeset Password'}
+					</Button>
+					{error && <InfoCard style="error" text={error.firebase} />}
+				</>
+			)}
 
-            {/* Display success message if the reset link was sent successfully */}
+			{/* Display success message if the reset link was sent successfully */}
 
-            {success && (
-                <div className={styles.successMessage}>
-                    <p>{success}</p>
-                </div>
-            )}
-        </form>
-    );
+			{success && (
+				<div className={styles.successMessage}>
+					<p>{success}</p>
+				</div>
+			)}
+		</form>
+	);
 };
 
 export default ForgotPasswordCard;
--- src/utils/formatFirebaseErrors.js
+++ src/utils/formatFirebaseErrors.js
@@ -5,10 +5,12 @@ const errorMessages = {
 	'auth/too-many-requests': 'You have tried to many times, please try again later',
 	'auth/operation-not-allowed': 'Please verify the new email before changing email',
 	'auth/email-already-in-use': 'This email is already in use',
+	'auth/user-not-found': 'User not found, please sign up',
 };
 
 export const formatFireBaseFeedback = (data, setError) => {
 	const formattedFeedback = { firebase: errorMessages[data] };
+	console.log(formattedFeedback);
 
 	return setError(formattedFeedback);
 };
